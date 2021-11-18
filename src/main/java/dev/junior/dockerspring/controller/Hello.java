b9e5d333068787e7314d4cfa61db60fd6c5e10a9
juniorvilas <log_junior@hotmail.com>
2021-11-18 17:32:17 -0300

    criado classe Hello

---
+++ src/main/java/dev/junior/dockerspring/controller/Hello.java
@@ -0,0 +1,16 @@
+package dev.junior.dockerspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String helloMessage(){
+        return "Hello World";
+    }
+
+
+
+}
